a8899135ff6394c2edc081dab711b7c2645b47e6
wwwshwww <www.shinderu.www@gmail.com>
2021-12-25 00:50:07 +0900

    add tests

---
+++ tests/env/test_base_visible.py
@@ -0,0 +1,19 @@
+import gym
+import navstack_gym
+import pytest
+
+@pytest.fixture(scope='module')
+def env(request):
+    env = gym.make('TreasureChestRoom-v1')
+    yield env
+
+@pytest.mark.commit 
+def test_initialization(env):
+    env.reset(is_generate_room=True, is_generate_pose=True)
+    done = False
+    for _ in range(10):
+        if not done:
+            action = env.action_space.sample()
+            observation, _, done, _ = env.step(action)
+
+    assert env.observation_space.contains(observation)
